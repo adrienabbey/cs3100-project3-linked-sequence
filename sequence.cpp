c5bac27a59bc05e12f1d973c9b0ca734fc26c1a9
Adrien Abbey <abbey.12@wright.edu>
2023-02-08 12:59:11 -0500

    finished basic commenting

--- sequence.cpp
+++ sequence.cpp
@@ -33,55 +33,71 @@ Sequence &Sequence::operator=(const Sequence &s)
 
 Sequence::value_type &Sequence::operator[](size_type position)
 {
-    // 
+    // Return a reference to the value of the given index position.
+    // NOTE: Throw an exception if the index value is invalid.
     throw exception();
 }
 
+/* Methods */
+
 void Sequence::push_back(const value_type &value)
 {
+    // Append the given value to the end of the Sequence, extending its size.
 }
 
 void Sequence::pop_back()
 {
+    // Delete the item at the end of the Sequence, reducing its size.
+    // NOTE: Throw an exception if there are no items in the Sequence.
     throw exception();
 }
 
 void Sequence::insert(size_type position, value_type value)
 {
+    // Insert the given value at the given position in the Sequence, extending its size.
     throw exception();
 }
 
 const Sequence::value_type &Sequence::front() const
 {
+    // Returns a reference to the first item in the Sequence.
+    // NOTE: Throws an exception if there are no items in the Sequence.
     throw exception();
 }
 
 const Sequence::value_type &Sequence::back() const
 {
+    // Returns a reference to the last item in the Sequence.
+    // NOTE: Throws an exception if there are no items in the Sequence.
     throw exception();
 }
 
 bool Sequence::empty() const
 {
+    // Return 'true' if this Sequence is empty, 'false' if not.
     return false;
 }
 
 Sequence::size_type Sequence::size() const
 {
+    // Return the size of this Sequence.
     return -1;
 }
 
 void Sequence::clear()
 {
+    // Delete all items in this sequence (freeing memory).
     throw exception();
 }
 
 void Sequence::erase(size_type position, size_type count)
 {
+    // Deletes the given number of items starting at the index value listed, reducing the Sequence size. 
     throw exception();
 }
 
 ostream &operator<<(ostream &os, const Sequence &s)
 {
+    // Returns an ostream output string of the given Sequence.
     return os;
 }
