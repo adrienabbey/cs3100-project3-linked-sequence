88d68e1a2cdda99c2ff07d186bc24e7e75c95e2e
Adrien Abbey <abbey.12@wright.edu>
2023-02-08 12:46:01 -0500

    started adding comments

--- sequence.cpp
+++ sequence.cpp
@@ -1,24 +1,39 @@
+/*
+ * Adrien Abbey, CS-3100-02, Feb. 8, 2023
+ * Project 3: Linked Sequence Data Structure
+ * Visual Studio Code Server, Ubuntu 22.04, g++/gdb
+ */
+
 #include "sequence.h"
+
+/* Constructors and Destructors */
 
 Sequence::Sequence(size_type sz)
 {
+    // Create a Sequence of the specified length.
 }
 
 Sequence::Sequence(const Sequence &s)
 {
+    // Create a deep copy of the given Sequence.
 }
 
 Sequence::~Sequence()
 {
+    // Destroy all objects belonging to this Sequence.
 }
+
+/* Operators */
 
 Sequence &Sequence::operator=(const Sequence &s)
 {
+    // Destroy objects belonging to this Sequence and replace them with a deep copy of the given Sequence.
     return *this;
 }
 
 Sequence::value_type &Sequence::operator[](size_type position)
 {
+    // 
     throw exception();
 }
 
