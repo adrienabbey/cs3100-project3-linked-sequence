61d001c0044f7cf927aa3a413c1beba87b77b368
Adrien Abbey <abbey.12@wright.edu>
2023-02-10 16:41:48 -0500

    personal test program

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Adrien Abbey, CS-3100-02, Feb. 8, 2023
+ * Project 3: Linked Sequence Data Structure
+ * Visual Studio Code Server, Ubuntu 22.04, g++/gdb
+ */
+
+#include <iostream>
+#include "sequence.h"
+
+using namespace std;
+
+int main()
+{
+    // Test the new sized Sequence constructor:
+    Sequence *newSizedSequence = new Sequence(5);
+
+    return 0;
+}
